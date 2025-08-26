0b721e3ef3de68e877b259d69e6179d14a60d4ef
ALEXANDRE F <alexandrepiata@gmail.com>
2025-08-25 22:03:19 -0300

    corrige error metro

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+config.resolver.sourceExts = ['js', 'json', 'ts', 'tsx', 'jsx'];
+
+config.transformer.minifierConfig = {
+  keep_fnames: true,
+  mangle: {
+    keep_fnames: true,
+  },
+};
+
+module.exports = config;
